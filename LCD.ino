e82780337fa2f782ecf1d6a5a9c4f34224964703
sarthak shinde <111444961+sarthakshinde20@users.noreply.github.com>
2023-11-19 21:05:25 +0530

    Update LCD.ino

--- LCD.ino
+++ LCD.ino
@@ -1,14 +1,14 @@
-#include<LiquidCrystal.h>
-LiquidCrystal LCD(12,11,5,4,3,2);
-void setup ()
-{
-  LCD.begin(16,2);
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(6,7,2,3,4,5);   //lcd object
+
+void setup() {
+  // put your setup code here, to run once:
+  lcd.begin(16, 2);
+  lcd.print("BE ECE");
 }
 
-void loop()
-{
-  LCD.setCursor(0,0);
-  delay(1000);
-  LCD.print("BE ECE");
-  delay(1000);
+void loop() {
+  // put your main code here, to run repeatedly:
+
 }
