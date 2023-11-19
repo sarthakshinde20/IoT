372655b5603c2db407c72e23a90fb38d1019717c
sarthak shinde <111444961+sarthakshinde20@users.noreply.github.com>
2023-11-19 21:04:30 +0530

    Update RGB_LED.ino

--- RGB_LED.ino
+++ RGB_LED.ino
@@ -1,38 +1,51 @@
-#define LED1 10
-#define LED2 11
-#define LED3 12
-#define LED4 13
+#define redpin 3
+#define greenpin 5
+#define bluepin 6
 
+int r = 255;
+int g = 0;
+int b = 0;
 void setup() {
-  // put your setup code here, to run once:
-  
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
-  pinMode(LED3, OUTPUT);
-  pinMode(LED4, OUTPUT);
-  
+  pinMode(redpin, OUTPUT);
+  pinMode(greenpin, OUTPUT);
+  pinMode(bluepin, OUTPUT);
+}
+
+void rgb(int r, int g, int b) {
+  analogWrite(redpin, r);
+  analogWrite(greenpin, g);
+  analogWrite(bluepin, b);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED1, HIGH);
-  delay(200);
-  digitalWrite(LED1, LOW);
-  delay(200);
-  
-  digitalWrite(LED2, HIGH);
-  delay(200);
-  digitalWrite(LED2, LOW);
-  delay(200);
-  
-  digitalWrite(LED3, HIGH);
-  delay(200);
-  digitalWrite(LED3, LOW);
-  delay(200);
-  
-  digitalWrite(LED4, HIGH);
-  delay(200);
-  digitalWrite(LED4, LOW);
-  delay(200);
-  
+  for (int i = 0; i < 255; i++) {
+    rgb(r, g, b);
+    g++;
+    delay(10);
+  }
+  for (int i = 0; i < 255; i++) {
+    rgb(r, g, b);
+    r--;
+    delay(10);
+  }
+  for (int i = 0; i < 255; i++) {
+    rgb(r, g, b);
+    b++;
+    delay(10);
+  }
+  for (int i = 0; i < 255; i++) {
+    rgb(r, g, b);
+    g--;
+    delay(10);
+  }
+  for (int i = 0; i < 255; i++) {
+    rgb(r, g, b);
+    r++;
+    delay(10);
+  }
+  for (int i = 0; i < 255; i++) {
+    rgb(r, g, b);
+    b--;
+    delay(10);
+  }
 }
