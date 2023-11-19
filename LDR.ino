5adcc02a070766e52fd8bd2aeb2d99386ecd9489
sarthak shinde <111444961+sarthakshinde20@users.noreply.github.com>
2023-11-19 21:03:10 +0530

    Update LDR.ino

--- LDR.ino
+++ LDR.ino
@@ -1,22 +1,15 @@
-const int ENA = 6;
-const int IN1 = 7;
-const int IN2 = 8;
-
+int ldrPin = A4; // Analog input pin for LDR int ldrValue; // Variable to store LDR value
 void setup()
 {
-  pinMode(ENA, OUTPUT);
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
+pinMode(ldrPin,INPUT);
+Serial.begin(9600); // Initialize serial communication for debugging
 }
 void loop()
 {
-  analogWrite(ENA,255);
-  digitalWrite(IN1,HIGH);
-  digitalWrite(IN2,LOW);
-  delay(2000);
-  
-  analogWrite(ENA,255);
-  digitalWrite(IN1,LOW);
-  digitalWrite(IN2,HIGH);
-  delay(2000);
+int readValue;
+float realValue;
+readValue = analogRead(ldrPin);
+realValue = (5.0/1024.0)*readValue;
+Serial.println(realValue);
+delay(1000);
 }
